fce039f2a4f010563e76d994cfb18f2386d9deb5
류형욱 <ryu@hyeonguk.com>
2019-04-08 00:58:51 +0900

    오류 처리 방식 개선

--- index.js
+++ index.js
@@ -11,29 +11,38 @@ function run(options, callback) {
         size = 10;
     }
     (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://iam.kaist.ac.kr/iamps/mobileLogin.do');
-        await page.waitForSelector('#id');
-        await page.waitForSelector('#password');
-        await page.type('#id', id);
-        await page.type('#password', password);
-        await page.click('.marg_bt22>a');
-        await page.waitForSelector('.user_info');
-        await page.goto('https://portal.kaist.ac.kr/index.html');
-        await page.goto(`https://portal.kaist.ac.kr/board/list.brd?boardId=today_notice&pageSize=${size}`);
-        const notices = await page.evaluate(() => {
-            const elements = document.querySelectorAll('.req_tit>a');
-            const notices = [];
-            elements.forEach(e => {
-                notices.push({
-                    title: e.text.trim(),
-                    link: 'https://portal.kaist.ac.kr' + e.href,
+        let browser = null;
+        try {
+            browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('https://iam.kaist.ac.kr/iamps/mobileLogin.do');
+            await page.waitForSelector('#id');
+            await page.waitForSelector('#password');
+            await page.type('#id', id);
+            await page.type('#password', password);
+            await page.click('.marg_bt22>a');
+            await page.waitForSelector('.user_info');
+            await page.goto('https://portal.kaist.ac.kr/index.html');
+            await page.goto(`https://portal.kaist.ac.kr/board/list.brd?boardId=today_notice&pageSize=${size}`);
+            const notices = await page.evaluate(() => {
+                const elements = document.querySelectorAll('.req_tit>a');
+                const notices = [];
+                elements.forEach(e => {
+                    notices.push({
+                        title: e.text.trim(),
+                        link: 'https://portal.kaist.ac.kr' + e.href,
+                    });
                 });
+                return notices;
             });
-            return notices;
-        });
-        await browser.close();
-        callback(null, notices);
+            await browser.close();
+            callback(null, notices);
+        } catch (e) {
+            callback(e);
+        } finally {
+            if (browser) {
+                await browser.close();
+            }
+        }
     })();
 }
